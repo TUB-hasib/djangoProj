6bfef82ddc1d449395efdb32f46bddaa648cb192
TUB-Hasib <h.alam@tu-berlin.de>
2024-07-15 16:07:21 +0200

    dividing code

--- test_project/test_app/controller.py
+++ test_project/test_app/controller.py
@@ -1,16 +1,29 @@
 from django.http import HttpResponse
 from openpyxl import load_workbook
+import json
 
-def read_excel_file(path):
-    wb = load_workbook(filename=path, rich_text=True, read_only=True )
-    sheet = wb.active
+def open_excel_file(path):
+    try:
+        # Load the workbook
+        workbook = load_workbook(filename=path, rich_text=True, read_only=True)
+        sheet = workbook.active
+        return sheet
 
+    except FileNotFoundError:
+        print("The file 'example.xlsx' was not found.")
+    except openpyxl.utils.exceptions.InvalidFileException:
+        print("The file 'example.xlsx' is not a valid Excel file.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
+def transform_excel_file(sheet):
+    #read header
     headers = []
     for cell in sheet[1]:
         val = cell.value if cell.value is not None else ''
         headers.append(str(val))
 
+    # read data 
     data_list = []
     for row in sheet.iter_rows(min_row=2, max_row=10): #strating from second row as 1st row is header
         row_val= []
@@ -20,20 +33,24 @@ def read_excel_file(path):
             row_val.append(str(val))
         data_list.append(row_val)
     
+    # ceating dict 
     data_dict = {}
     data_dict['headers'] = headers
     data_dict['value'] = data_list
 
+    json_data = json.dumps(data_dict)
+
     return data_dict
 
-def convert_excel_to_json():
-    pass
 
-def document_view(url):
-    data = read_excel_file(url)
+
+def read_document(url):
+    data_sheet = open_excel_file(url)
+    data = transform_excel_file(data_sheet)
     # return HttpResponse(f"Hello Geeks {data}")
     print(data)
+    print(type(data))
 
 
 exel_file_path = './static/the-office-lines.xlsx'
-document_view(exel_file_path)
+read_document(exel_file_path)
